3efda7e2b245b36883b23e824771b6fce1fe356c
Elizabeth Kushelevsky <egkushelevsky@gmail.com>
2024-10-11 19:44:43 -0400

    Commented code

--- uecho.c
+++ uecho.c
@@ -1,46 +1,57 @@
-  1 #include <stdio.h>
-  2 #include <stdlib.h>
-  3 #include <string.h>
-  4 #include <ctype.h>
-  5 
-  6 void uecho(char **argv) { //Print CL args
-  7     char **p = argv;
-  8     *p++;
-  9     while(*p) {
- 10         printf("%s\n", *p++);
- 11     }
- 12 }
- 13 
- 14 void recho(int argc, char **argv) { //Print CL args in reverse
- 15     char **p = argv-1;
- 16     *p = NULL;
- 17     *p++;
- 18     p += (argc-1);
- 19 
- 20     while(*(p-1)) {
- 21         printf("%s\n", *p--);
- 22     }
- 23 }
- 24 
- 25 void cecho(char **argv) { //Print the first character of each arg
- 26     char **p = argv;
- 27     *p++;
- 28     while(*p) {
- 29         putchar(**p);
- 30         putchar('\n');
- 31         *p++;
- 32     }
- 33 }
- 34 
- 35 void clecho(char **argv) { //Print the last char of each arg
- 36     char **p = argv;
- 37     *p++;
- 38     while(*p) {
- 39         char *str = *p++;
- 40         while(*(str+1)) {
- 41             str++;
- 42         }
- 43         putchar(*str);
- 44         putchar('\n');
- 45     }
- 46 }
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+void uecho(char **argv) { //Print CL args
+    char **p = argv;
+    *p++;
+    while(*p) {
+        printf("%s\n", *p++);
+    }
+}
+
+void recho(int argc, char **argv) { //Print CL args in reverse
+    char **p = argv-1;
+    *p = NULL;
+    *p++;
+    p += (argc-1);
+
+    while(*(p-1)) {
+        printf("%s\n", *p--);
+    }
+}
+
+void cecho(char **argv) { //Print the first character of each arg
+    char **p = argv;
+    *p++;
+    while(*p) {
+        putchar(**p);
+        putchar('\n');
+        *p++;
+    }
+}
+
+void clecho(char **argv) { //Print the last char of each arg
+    char **p = argv;
+    *p++;
+    while(*p) {
+        char *str = *p++;
+        while(*(str+1)) {
+            str++;
+        }
+        putchar(*str);
+        putchar('\n');
+    }
+}
+
+int main(int argc, char **argv) {
+    
+   uecho(argv);
+   printf("\n");
+   recho(argc, argv);
+   printf("\n");
+   cecho(argv);
+   printf("\n");
+   clecho(argv);
+}  
