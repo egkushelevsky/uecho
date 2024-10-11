a9c179359a9e6ccec3659b5bd2a15877693f6ba4
Elizabeth Kushelevsky <egkushelevsky@gmail.com>
2024-10-11 13:49:14 -0400

    Working print args in reverse order

--- uecho.c
+++ uecho.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+  1 #include <stdio.h>
   2 #include <stdlib.h>
   3 #include <string.h>
   4 #include <ctype.h>
@@ -9,9 +9,23 @@
   9     while(*p) {
  10         printf("%s\n", *p++);
  11     }   
- 12 }   
+ 12 }
  13 
- 14 int main(int argc, char **argv) {
- 15    
- 16    uecho(argv); 
- 17 }
+ 14 void recho(int argc, char **argv) {
+ 15     char **p = argv-1;
+ 16     *p = NULL;
+ 17     *p++;
+ 18     p += (argc-1);
+ 19     
+ 20     while(*(p-1)) {
+ 21         printf("%s\n", *p--);
+ 22     }
+ 23 }
+ 24 
+ 25 int main(int argc, char **argv) {
+ 26 
+ 27    uecho(argv);
+ 28    printf("\n");
+ 29    recho(argc, argv);
+ 30 }
+
